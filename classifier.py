2b13b74ecdde42d48ac4f319b455b083aa394ae9
niraj9493 <35814094+niraj9493@users.noreply.github.com>
2018-12-04 09:20:07 -0800

    Python Port
    nstall SciKit- learn and skimage using pip

---
+++ classifier.py
@@ -0,0 +1,321 @@
+'''
+This script performs the basic process for applying a machine learning
+algorithm to a dataset using Python libraries.
+
+The four steps are:
+   1. Download a dataset (using pandas)
+   2. Process the numeric data (using numpy)
+   3. Train and evaluate learners (using scikit-learn)
+   4. Plot and compare results (using matplotlib)
+
+
+The data is downloaded from URL, which is defined below. As is normal
+for machine learning problems, the nature of the source data affects
+the entire solution. When you change URL to refer to your own data, you
+will need to review the data processing steps to ensure they remain
+correct.
+
+============
+Example Data
+============
+The example is from http://mlr.cs.umass.edu/ml/datasets/Spambase
+It contains pre-processed metrics, such as the frequency of certain
+words and letters, from a collection of emails. A classification for
+each one indicating 'spam' or 'not spam' is in the final column.
+See the linked page for full details of the data set.
+
+This script uses three classifiers to predict the class of an email
+based on the metrics. These are not representative of modern spam
+detection systems.
+'''
+
+# Remember to update the script for the new data when you change this URL
+URL = "http://mlr.cs.umass.edu/ml/machine-learning-databases/spambase/spambase.data"
+
+# Uncomment this call when using matplotlib to generate images
+# rather than displaying interactive UI.
+#import matplotlib
+#matplotlib.use('Agg')
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2 as cv
+import os 
+import sys
+from skimage.feature import hog
+
+# =====================================================================
+def Segment(image):
+    ch = [ i for i in cv.split(image)]
+
+    for i in range(len(ch)):
+        ret, ch[i] = cv.threshold(ch[i],0,255,cv.THRESH_BINARY_INV +cv.THRESH_OTSU)
+
+    mask = ch[0].astype(np.uint32) + ch[1].astype(np.uint32) + ch[2].astype(np.uint32)
+    mask = mask/3
+    mask = mask.astype(np.uint8)
+    _,mask = cv.threshold(mask,85,255,cv.THRESH_BINARY)
+    return mask
+
+def feature_extract(img, mask):
+    #hog = cv.HOGDescriptor()
+    mask = cv.morphologyEx(mask,cv.MORPH_OPEN,kernel=cv.getStructuringElement(cv.MORPH_RECT,(4,4)))
+    mask = cv.medianBlur(mask,7)
+    mask = cv.medianBlur(mask,5)
+    mask = cv.medianBlur(mask,3)
+    mask,conts,_ = cv.findContours(mask,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
+    mask = img * mask[:,:,None].astype(img.dtype)
+    mask = cv.drawContours(mask, conts, -1, (0,255,0), 3)
+    fd, hog_image = hog(mask, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualise=True)
+    cv.imshow('name',hog_image)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    return hog.compute(mask)
+
+
+def get_features_and_labels():
+    '''
+    Transforms and scales the input data and returns numpy arrays for
+    training and testing inputs and targets.
+    '''
+
+    # Replace missing values with 0.0, or we can use
+    # scikit-learn to calculate missing values (below)
+    #frame[frame.isnull()] = 0.0
+
+    # Convert values to floats
+    #arr = np.array(frame, dtype=np.float)
+
+    X = []
+    y = []
+    p_size = 0
+    n_size = 0
+    dir_p = r"C:/Users/Niraj93/source/repos/Melanoma_Detection/Melanoma_Detection/data/data/data_positive/"
+    dir_n = r"C:/Users/Niraj93/source/repos/Melanoma_Detection/Melanoma_Detection/data/data/data_negative/"
+    for f in os.listdir(dir_p):
+       if f.endswith('.jpg'):
+           #files.append(dir_p+f)
+           img = cv.imread(dir_p+f)
+           img = cv.resize(img,(640,480))
+           if img is None:
+                raise Exception("No Image Loaded !")
+           #X.append(np.array(feature_extract(img,Segment(img))))
+           X.append(feature_extract(img,Segment(img)))
+           #y.append(np.array(1))
+           y.append(1)
+           #TrainingSamples.append(feature_extract(img,Segment(img)))
+           #label.append(1)
+
+    for f in os.listdir(dir_n):
+       if f.endswith('.jpg'):
+           #files.append(dir_n+f)
+           img = cv.imread(dir_n+f)
+           img = cv.resize(img,(640,480))
+           if img is None:
+                raise Exception("No Image Loaded !")
+           X.append(feature_extract(img,Segment(img)))
+           y.append(-1)
+           #TrainingSamples.append(feature_extract(img,Segment(img)))
+           #label.append(-1)
+    # Use the last column as the target value
+    #X, y = arr[:, :-1], arr[:, -1]
+    # To use the first column instead, change the index value
+    #X, y = arr[:, 1:], arr[:, 0]
+    
+    # Use 80% of the data for training; test against the rest
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    # sklearn.pipeline.make_pipeline could also be used to chain 
+    # processing and classification into a black box, but here we do
+    # them separately.
+    
+    # If values are missing we could impute them from the training data
+    #from sklearn.preprocessing import Imputer
+    #imputer = Imputer(strategy='mean')
+    #imputer.fit(X_train)
+    #X_train = imputer.transform(X_train)
+    #X_test = imputer.transform(X_test)
+    
+    # Normalize the attribute values to mean=0 and variance=1
+    from sklearn.preprocessing import StandardScaler
+    scaler = StandardScaler()
+    # To scale to a specified range, use MinMaxScaler
+    #from sklearn.preprocessing import MinMaxScaler
+    #scaler = MinMaxScaler(feature_range=(0, 1))
+    
+    # Fit the scaler based on the training data, then apply the same
+    # scaling to both training and test sets.
+    #nsamples, nx, ny = X_train.shape
+    #d2_train_dataset = X_train.reshape((nsamples,nx*ny))  
+    scaler.fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    # Return the training and test sets
+    return X_train, X_test, y_train, y_test
+
+
+# =====================================================================
+
+
+def evaluate_classifier(X_train, X_test, y_train, y_test):
+    '''
+    Run multiple times with different classifiers to get an idea of the
+    relative performance of each configuration.
+
+    Returns a sequence of tuples containing:
+        (title, precision, recall)
+    for each learner.
+    '''
+
+    # Import some classifiers to test
+    from sklearn.svm import LinearSVC, NuSVC
+    from sklearn.ensemble import AdaBoostClassifier
+
+    # We will calculate the P-R curve for each classifier
+    from sklearn.metrics import precision_recall_curve, f1_score
+    
+    # Here we create classifiers with default parameters. These need
+    # to be adjusted to obtain optimal performance on your data set.
+    
+    # Test the linear support vector classifier
+    classifier = LinearSVC(C=1)
+    # Fit the classifier
+    #classifier.fit(X_train, y_train)
+    score = f1_score(y_test, classifier.predict(X_test))
+    # Generate the P-R curve
+    y_prob = classifier.decision_function(X_test)
+    precision, recall, _ = precision_recall_curve(y_test, y_prob)
+    # Include the score in the title
+    yield 'Linear SVC (F1 score={:.3f})'.format(score), precision, recall
+
+    # Test the Nu support vector classifier
+    classifier = NuSVC(kernel='rbf', nu=0.5, gamma=1e-3)
+    # Fit the classifier
+    #classifier.fit(X_train, y_train)
+    score = f1_score(y_test, classifier.predict(X_test))
+    # Generate the P-R curve
+    y_prob = classifier.decision_function(X_test)
+    precision, recall, _ = precision_recall_curve(y_test, y_prob)
+    # Include the score in the title
+    yield 'NuSVC (F1 score={:.3f})'.format(score), precision, recall
+
+    # Test the Ada boost classifier
+    classifier = AdaBoostClassifier(n_estimators=50, learning_rate=1.0, algorithm='SAMME.R')
+    # Fit the classifier
+    classifier.fit(X_train, y_train)
+    score = f1_score(y_test, classifier.predict(X_test))
+    # Generate the P-R curve
+    y_prob = classifier.decision_function(X_test)
+    precision, recall, _ = precision_recall_curve(y_test, y_prob)
+    # Include the score in the title
+    yield 'Ada Boost (F1 score={:.3f})'.format(score), precision, recall
+
+# =====================================================================
+
+
+def plot(results):
+    '''
+    Create a plot comparing multiple learners.
+
+    `results` is a list of tuples containing:
+        (title, precision, recall)
+    
+    All the elements in results will be plotted.
+    '''
+
+    # Plot the precision-recall curves
+
+    fig = plt.figure(figsize=(6, 6))
+    fig.canvas.set_window_title('Classifying data from ' + URL)
+
+    for label, precision, recall in results:
+        plt.plot(recall, precision, label=label)
+
+    plt.title('Precision-Recall Curves')
+    plt.xlabel('Precision')
+    plt.ylabel('Recall')
+    plt.legend(loc='lower left')
+
+    # Let matplotlib improve the layout
+    plt.tight_layout()
+
+    # ==================================
+    # Display the plot in interactive UI
+    plt.show()
+
+    # To save the plot to an image file, use savefig()
+    #plt.savefig('plot.png')
+
+    # Open the image file with the default image viewer
+    #import subprocess
+    #subprocess.Popen('plot.png', shell=True)
+
+    # To save the plot to an image in memory, use BytesIO and savefig()
+    # This can then be written to any stream-like object, such as a
+    # file or HTTP response.
+    #from io import BytesIO
+    #img_stream = BytesIO()
+    #plt.savefig(img_stream, fmt='png')
+    #img_bytes = img_stream.getvalue()
+    #print('Image is {} bytes - {!r}'.format(len(img_bytes), img_bytes[:8] + b'...'))
+
+    # Closing the figure allows matplotlib to release the memory used.
+    plt.close()
+
+
+# =====================================================================
+
+
+if __name__ == '__main__':
+
+    #X_train = []
+    #Y_train = []
+
+    #p_size = 0
+    #n_size = 0
+    #dir_p = r"C:/Users/Niraj93/source/repos/Melanoma_Detection/Melanoma_Detection/data/data/data_positive/"
+    #dir_n = r"C:/Users/Niraj93/source/repos/Melanoma_Detection/Melanoma_Detection/data/data/data_negative/"
+    #for f in os.listdir(dir_p):
+    #   if f.endswith('.jpg'):
+    #       #files.append(dir_p+f)
+    #       img = cv.imread(dir_p+f)
+    #       img = cv.resize(img,(640,480))
+    #       if img is None:
+    #            raise Exception("No Image Loaded !")
+    #       X_train.append(np.array(feature_extract(img,Segment(img))))
+    #       Y_train.append(np.array(1))
+    #       #TrainingSamples.append(feature_extract(img,Segment(img)))
+    #       #label.append(1)
+
+    #for f in os.listdir(dir_n):
+    #   if f.endswith('.jpg'):
+    #       #files.append(dir_n+f)
+    #       img = cv.imread(dir_n+f)
+    #       img = cv.resize(img,(640,480))
+    #       if img is None:
+    #            raise Exception("No Image Loaded !")
+    #       X_train.append(np.array(feature_extract(img,Segment(img))))
+    #       Y_train.append(np.array(-1))
+    #       #TrainingSamples.append(feature_extract(img,Segment(img)))
+    #       #label.append(-1)
+            
+
+   
+    #cv.imshow('name',mask)
+    #cv.waitKey(0)
+    #cv.destroyAllWindows()
+
+
+    
+    # Process data into feature and label arrays
+    X_train, X_test, y_train, y_test = get_features_and_labels()
+
+    # Evaluate multiple classifiers on the data
+    print("Evaluating classifiers")
+    results = list(evaluate_classifier(X_train, X_test, y_train, y_test))
+
+    # Display the results
+    print("Plotting the results")
+    plot(results)
